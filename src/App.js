30ae8f3050ce0f4ed6f66b3f97af98477c1a1d9d
Claudia Font <claudiafontmarques@gmail.com>
2021-10-18 16:11:22 +0200

    Added array of circles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [status, setStatus] = useState("active");
+  const circlesNum = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+
+  return circlesNum.map((circle) => (
+    <Circle key={circlesNum.id} className={status} />
+  ));
 }
 
 export default App;
